76d1c7b404ce50bddd0721242c8083836e6c62e8
eliza29 <elizaseguraf@gmail.com>
2018-02-19 18:23:41 -0500

    mostrando topics de la data

--- js/main.js
+++ js/main.js
@@ -22,3 +22,27 @@ $(document).ready(() => {
     btnNewTopic.on('click', showFormNewTopic);
     btnCreateTopic.on('click', createNewTopic);
 });
+// hacer funcionar con ajax
+let $addTopic = $('.add-topic')
+    $addTopic.append('<div class="row justify-content-center box"/>');
+    
+function addResponse(data) {
+  console.log('the ajax request has finished!');
+  console.log(data);
+  $.each(data,function(i,obj){
+    let aTopic = data[i].content;
+    let anAuthor = data[i].author_name;
+    let numberAnswers = data[i].responses_count;
+    $addTopic.find('.box').append(`<div class="col-8 p-3 my-3 border rounded">
+    <div class="row justify-content-center"><div class="col-4"><p class="text-center my-0">Tema:
+    <span id="topic">${aTopic}</span></p></div><div class="col-4"><p class="text-center my-0">Por:
+    <span id="author">${anAuthor}</span></p></div><div class="col-4"><p class="text-center my-0">Respuestas:
+    <span id="responses-number">${numberAnswers}</span></p></div></div></div></div>`);                          
+   }) 
+}
+$.ajax({
+  url: `http://examen-laboratoria-sprint-5.herokuapp.com/topics`
+}).done(addResponse)
+.fail(function() {
+  alert( "error" );
+});
